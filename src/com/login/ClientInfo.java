542d01ce060b5bffd8d26a582a260979059b9b4e
Jerosin93 <jmjpjoseph28593@gmail.com>
2022-09-05 12:41:04 +0530

    new

---
+++ src/com/login/ClientInfo.java
@@ -0,0 +1,14 @@
+package com.login;
+
+public class ClientInfo {
+	public static void main(String args[]) {
+		// index 0123456789------n-1
+		String j = "Welcome to India";
+		System.out.println(j);
+
+		int length = j.length();
+		System.out.println(length);
+
+	}
+
+}
